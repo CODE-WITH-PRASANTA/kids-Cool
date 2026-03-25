ee2fa2e3c55ed05c9a677bf4468cbf7377b1a145
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-25 14:14:44 +0530

    Navbar , Footer Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-
   return (
- <>
- 
- </>
-  )
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/why" element={<h1>Why</h1>} />
+        <Route path="/history" element={<h1>History</h1>} />
+        <Route path="/stage" element={<h1>Stage</h1>} />
+        <Route path="/teachers" element={<h1>Teachers</h1>} />
+        <Route path="/programms" element={<h1>Programms</h1>} />
+        <Route path="/gallery" element={<h1>Gallery</h1>} />
+        <Route path="/news" element={<h1>News</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
