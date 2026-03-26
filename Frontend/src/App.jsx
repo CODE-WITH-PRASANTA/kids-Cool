41d5d5e656dd2d508e7ed7b336d69679f6b5b7e2
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-26 11:08:28 +0530

    Pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { Routes, Route } from "react-router-dom";
-import Homehero from "./Components/Homehero/Homehero";
-import Home from "./Pages/Home/Home";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  );
-};
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
@@ -39,6 +26,5 @@ function App() {
     </BrowserRouter>
   );
 }
->>>>>>> 6b71545aae3711683b78aad5119e4b57858ade90
 
 export default App;
