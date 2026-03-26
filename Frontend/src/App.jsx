73e7b37f8c78c0ca11e7fe6968bcd2bf58efe552
Ankitanayak28 <153258452+Ankitanayak28@users.noreply.github.com>
2026-03-26 11:07:44 +0530

    push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,21 @@
 import React from "react";
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
-import Homehero from "./Components/Homehero/Homehero";
-import Home from "./Pages/Home/Home";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  );
-};
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
+import Home from "./Pages/Home/Home";
+
 
 function App() {
   return (
-    <BrowserRouter>
-
+    <>
       <Navbar />
-
       <Routes>
-        <Route path="/" element={<Home/>} />
-        {/* <Route path="/why" element={<Why/>} />
-        <Route path="/history" element={<History/>} />
-        <Route path="/stage" element={<Stage/>} />
-        <Route path="/teachers" element={<Teachers/>} />
-        <Route path="/programms" element={<Programms/>} />
-        <Route path="/gallery" element={<Gallery/>} />
-        <Route path="/news" element={<News/>} />
-        <Route path="/contact" element={<Contact/>} /> */}
+        <Route path="/" element={<Home/>}/>
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </>
   );
 }
->>>>>>> 6b71545aae3711683b78aad5119e4b57858ade90
 
 export default App;
